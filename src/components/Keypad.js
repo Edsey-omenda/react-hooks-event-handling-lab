d03e4501be67ee1aa2aa4f0d1f54ec93ce97df03
Edsey omenda <omendajames0317@gmail.com>
2022-07-15 16:42:35 +0300

    creates keypad component and uses it to render the enterring password image

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad(){
+
+    function handleChange(event) {
+        console.log("Entering password...");
+      }
+    
+      return (
+        <div>
+         <input type="password"  onChange={handleChange} name="password" placeholder="Enter password..." />
+        </div>
+      );
+}
+
+export default Keypad;
